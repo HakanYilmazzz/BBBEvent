a719eb0337f285b24ca7c3c7bd28886e429a3ced
Yilmaz718 <hakanyilmazzz93@gmail.com>
2025-01-15 18:15:07 +0300

    Güncelleme: Değişiklikler eklendi

---
+++ config.js
@@ -0,0 +1,9 @@
+const config = {
+    API_URL: 'https://biletinial.com/GetAllEventsByCity?cityId=25&langId=1&countryId=3&langCode=tr',
+    PROXY_URL: 'https://cors-anywhere.herokuapp.com/',
+    IMAGE_BASE_URL: 'https://b6s54eznn8xq.merlincdn.net',
+    SITE_BASE_URL: 'https://biletinial.com'
+};
+
+// Config'i global olarak kullanılabilir yap
+window.config = config; 
